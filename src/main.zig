a6db8e6d9302ff8e73dc0c2c3c470abc101d5205
PaNDa2Code <moaaz0688@gmail.com>
2024-11-27 21:03:37 +0200

    Update to latest zig

--- src/main.zig
+++ src/main.zig
@@ -376,7 +376,7 @@ pub fn main() !void {
 
             try stdout_w.writeAll("\n");
 
-            inline for (@typeInfo(Command.Measurements).@"struct".fields) |field| {
+            inline for (@typeInfo(Command.Measurements).Struct.fields) |field| {
                 const measurement = @field(command.measurements, field.name);
                 const first_measurement = if (command_n == 1)
                     null
